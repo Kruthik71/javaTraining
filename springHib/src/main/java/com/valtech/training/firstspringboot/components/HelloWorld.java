5a3fe397b4d18710ef37e0de6193d02352595df6
Kruthik71 <kruthikbhupal7@gmail.com>
2023-11-08 15:49:11 +0530

    Cool

---
+++ springHib/src/main/java/com/valtech/training/firstspringboot/components/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.valtech.training.firstspringboot.components;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class HelloWorld {
+	public String Hello() {
+		return "Kruthik Welcomes you to SpringBoot";
+	}
+}
